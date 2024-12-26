aedab7ab6c87dd295ffe4c24647ebb696b45ea3c
Arun7286 <19kb1a1222@nbkrist.org>
2024-12-26 19:38:05 +0530

    states in React

--- learnstates/src/Counter.jsx
+++ learnstates/src/Counter.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { useState } from "react";
 export default function Counter() {
     const [count, setCount] =useState(0);
+
+    const increase = () => {
+        setCount(count + 1);
+    }
     return (
         <div>
             <h1>{ count }</h1>
-            <button onClick = {() => {setCount(count+1)}}>+</button>
+            <button onClick = {increase}>+</button>
             <button onClick  = {() => {setCount(count-1)}}>-</button>
             <br></br><br></br>
             <button onClick = {() => {setCount(0)}}>Reset</button>
