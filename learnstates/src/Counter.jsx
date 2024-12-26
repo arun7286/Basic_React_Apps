0779e874851a2d5ebf3c02c5477e4fb32ebb3eb9
Arun7286 <19kb1a1222@nbkrist.org>
2024-12-26 13:34:10 +0530

    states in React

--- learnstates/src/Counter.jsx
+++ learnstates/src/Counter.jsx
@@ -4,7 +4,7 @@ export default function Counter() {
     const [count, setCount] =useState(0);
     return (
         <div>
-            <h1>{count}</h1>
+            <h1>{ count }</h1>
             <button onClick = {() => {setCount(count+1)}}>+</button>
             <button onClick  = {() => {setCount(count-1)}}>-</button>
             <br></br><br></br>
