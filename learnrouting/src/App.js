facc051f2a40ac276621e8ce756df4490740d0bb
Arun7286 <19kb1a1222@nbkrist.org>
2024-12-30 13:18:44 +0530

    Routing in react

--- learnrouting/src/App.js
+++ learnrouting/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import {Link, Route, Routes} from 'react-router-dom';
+import Contact from "./Contact";
+import About from "./About";
+import Home from "./Home";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <button><Link to= '/'> Home </Link></button>
+          <button><Link to= '/about'> About </Link></button>
+          <button><Link to= '/contact'> Contact </Link></button>
+        <Routes>
+            <Route path = '/' element = {<Home />}> </Route>
+            <Route path = '/about' element = {<About />}> </Route>
+            <Route path = '/contact' element = {<Contact />}> </Route>
+        </Routes>
+      </div>
+  )
 }
-
-export default App;
